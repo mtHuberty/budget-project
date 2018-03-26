4b8958715c8b58ce3b473945f363bc4d211d65d5
mth1004 <matthew.huberty@daugherty.com>
2018-03-26 14:23:42 -0500

    Initialized git repo

---
+++ budget-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExpendituresList from './expenditures-list';
+
+ReactDOM.render(<ExpendituresList />, document.getElementById('root'));
